db2f4f203f937270ff85fffa9620857edcf883e8
KrzysztofAdam97 <k.guzenda1@gmail.com>
2022-04-18 22:14:29 +0200

    klasy

---
+++ shop-react-.net-back/Entities/Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace shop_react_.net_back.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; } // long/decimal/int?
+        public string ImageUrl { get; set; }
+        public string Type { get; set; } // nowe/uzywane/open_box?
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string Stock { get; set; } // stan magazynowy
+    }
+}
---
+++ shop-react-.net-back/Entities/User.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace shop_react_.net_back.Entities
+{
+    public class User : IdentityUser
+    {
+        
+    }
+}
