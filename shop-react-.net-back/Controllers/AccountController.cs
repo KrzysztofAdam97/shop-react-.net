51e5fcb11655a1e9e6bf6363552367186d2aa7fd
KrzysztofAdam97 <k.guzenda1@gmail.com>
2022-04-20 17:59:46 +0200

    acc control

--- shop-react-.net-back/Controllers/AccountController.cs
+++ shop-react-.net-back/Controllers/AccountController.cs
@@ -1,10 +1,17 @@
 using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using shop_react_.net_back.Entities;
 
 namespace shop_react_.net_back.Controllers
 {
     public class AccountController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
         
     }
 }
