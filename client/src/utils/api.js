9eea163211402ea17d895caa9c1c3a03b6fe07df
Sebastian Smela <sebastiansmela@sebastians-macbook-pro.home>
2022-06-11 00:12:27 +0200

    http instead of https

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 
 const config = {
-  baseURL: "https://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
